70770a790641e1ceaf2567a91a0d479432296f8d
yolizon <75636853+yolizon@users.noreply.github.com>
2021-11-01 23:26:11 +0200

    Add files via upload

---
+++ config/webpack.common.js
@@ -0,0 +1,52 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackBar = require('webpackbar');
+const paths = require('./paths')
+
+module.exports = {
+  entry: [paths.src + '/index.js'],
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: paths.src + '/template.html', // template file
+      filename: 'index.html', // output file
+    }),
+    new WebpackBar()
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, use: ['babel-loader'] },
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+      { test: /\.hbs$/, use: 'handlebars-loader' },
+      { test: /\.html$/i, use: 'html-loader'},
+    ],
+  },
+
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': paths.src,
+      assets: paths.public,
+    },
+  },
+}
